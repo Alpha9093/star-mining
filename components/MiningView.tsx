2c68c3cde95b766665d6337fee2274185c7e239a
Alpha9093 <165902058+Alpha9093@users.noreply.github.com>
2026-01-09 20:26:06 +0530

    Create MiningView.tsx

---
+++ components/MiningView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MiningView = () => {
+  return (
+    <div>
+      <h1>Mining View Component</h1>
+      <p>This is a placeholder for MiningView component.</p>
+    </div>
+  );
+};
+
+export default MiningView;
